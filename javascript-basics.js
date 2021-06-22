49e84fee3b2bff88adc62cc71ac8604676fc6bc4
vabhi <cvabhi94@gmail.com>
2021-06-22 09:30:21 -0700

    variable exercise

--- javascript-basics.js
+++ javascript-basics.js
@@ -109,3 +109,249 @@ alert(sum);
 undefined
 sum
 10
+//  exercise make a calculator 
+
+custom_background_image:1 The page delivered both an 'X-Frame-Options' header and a 'Content-Security-Policy' header with a 'frame-ancestors' directive. Although the 'X-Frame-Options' header alone would have blocked embedding, it has been ignored.
+custom_background_image:1 The page delivered both an 'X-Frame-Options' header and a 'Content-Security-Policy' header with a 'frame-ancestors' directive. Although the 'X-Frame-Options' header alone would have blocked embedding, it has been ignored.
+one-google-bar:1 The page delivered both an 'X-Frame-Options' header and a 'Content-Security-Policy' header with a 'frame-ancestors' directive. Although the 'X-Frame-Options' header alone would have blocked embedding, it has been ignored.
+app:1 The page delivered both an 'X-Frame-Options' header and a 'Content-Security-Policy' header with a 'frame-ancestors' directive. Although the 'X-Frame-Options' header alone would have blocked embedding, it has been ignored.
+prompt()
+"hiii m"
+name = "hiii"
+"hiii"
+var george = "these pretzels are making me thirsty" + "!"
+undefined
+george
+"these pretzels are making me thirsty!"
+var george = "hiiii" + "name";
+undefined
+george
+"hiiiiname"
+6ss = "hii"
+VM3108:1 Uncaught SyntaxError: Invalid or unexpected token
+ss6 = "hii"
+"hii"
+_name = "hii"
+"hii"
+prompt()
+"halooo"
+firstname = "abhi"
+"abhi"
+firstName = "abhi"
+"abhi"
+first_name = "abhi"
+"abhi"
+prompt("what is your yser name")
+"abhi"
+prompt("what is your last name")
+"chebrolu"
+var firstLetter = prompt("enter your first number")
+undefined
+var first = prompt('enter your first number")
+VM3699:1 Uncaught SyntaxError: Invalid or unexpected token
+var first = prompt("enter your first number")
+undefined
+var second = prompt("enter your second number"
+VM3808:1 Uncaught SyntaxError: missing ) after argument list
+var first = prompt('enetr your first number");
+VM3909:1 Uncaught SyntaxError: Invalid or unexpected token
+var first = prompt("enetr your first number");
+undefined
+var first = prompt("enetr your first number"); var second = prompt("second num")
+undefined
+first
+"1"
+second
+"2"
+first
+"1"
+second
+"2"
+number(first)
+VM4092:1 Uncaught ReferenceError: number is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM4092:1
+Number(first)
+1
+Number(second)
+2
+Number(first) + Number(second)
+3
+var sum = Number(first) +m Number(second)
+VM4472:1 Uncaught SyntaxError: Unexpected identifier
+var sum = Number(first) + Number(second)
+undefined
+sum
+3
+aletr('hiii')
+VM4715:1 Uncaught ReferenceError: aletr is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM4715:1
+alert()
+undefined
+alert("hii there")
+undefined
+alert("hiiii")
+undefined
+alert("my name is abhi")
+undefined
+var first = prompt("enter first number");
+var second = prompt("enter second number);
+var sum = Number(first) + Number(second);
+alert(sum)
+VM4874:2 Uncaught SyntaxError: Invalid or unexpected token
+var first = prompt("enter first number");
+var second = prompt("enter second number);
+var sum = Number(first) + Number(second);
+alert(sum);
+VM4882:2 Uncaught SyntaxError: Invalid or unexpected token
+var first = prompt("enter first number");
+var second = prompt("enter second number");
+var sum = Number(first) + Number(second);
+alert(sum)
+undefined
+var first = prompt("enter first number");
+var second = prompt("enter second number);
+var sum = Number(first) + Number(second);
+alert(sum);
+VM4898:2 Uncaught SyntaxError: Invalid or unexpected token
+var first = prompt("enter first number");
+var second = prompt("enter second number");
+var sum = Number(first) + Number(second);
+alert(sum);
+undefined
+sum
+10
+var firstname = prompt("your first name");
+var lastname = prompt("last name");
+var sum = String(firstname) + string(lastname);
+VM5323:3 Uncaught ReferenceError: string is not defined
+    at <anonymous>:3:11
+(anonymous) @ VM5323:3
+var firstname = prompt("your first name");
+var lastname = prompt("last name");
+var sum = (firstname) + (lastname);
+alert("your name is": + sum)
+VM5421:4 Uncaught SyntaxError: missing ) after argument list
+var firstname = prompt("your first name");
+var lastname = prompt("last name");
+var sum = (firstname) + (lastname);
+undefined
+sum
+"hiiijjkk"
+var a = 34;
+var b = 21:
+VM5509:2 Uncaught SyntaxError: Unexpected token ':'
+var a = 34;
+var b= 21;
+a = 2;
+var c = a +blur
+2
+var a = 34;
+var b= 21;
+a = 2;
+var c = a + b;
+2
+var a = 34;
+var b= 21;
+a = 2;
+2
+var a = 34;
+var b= 21;
+a = 2;
+b + a 
+23
+var c
+undefined
+var first = "abhi"
+undefined
+var first = "abhi";
+var second = "ram";
+var full = first + second;
+undefined
+full
+"abhiram"
+var first = "abhi";
+var second = "ram";
+var full = first + "" +  second;
+undefined
+full
+"abhiram"
+var first = "abhi";
+var second = "ram";
+var full = first + " " + second;
+undefined
+full
+"abhi ram"
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+var sum = first + second ;
+alert(sum);
+undefined
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first) * (second));
+
+undefined
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+var sum = first + second ;
+alert(sumvar first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+var sum = first + second ;
+alert(sumvar first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+var sum = first + second ;
+alert(sumvar first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+var sum = first + second ;
+alert(sumvar first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+var sum = first + second ;
+alert(sumvar first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first)+(second));
+undefined
+VM6390:4 Uncaught SyntaxError: missing ) after argument list
+VM6391:4 Uncaught SyntaxError: missing ) after argument list
+VM6392:4 Uncaught SyntaxError: missing ) after argument list
+VM6393:4 Uncaught SyntaxError: missing ) after argument list
+VM6394:4 Uncaught SyntaxError: missing ) after argument list
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first) * (second));
+
+undefined
+alert
+ƒ alert() { [native code] }
+alert()
+undefined
+var first = prompt("enter first num");
+var second = prompt("enter second num");
+alert((first) * (second));
+undefined
