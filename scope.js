5ab26cc94e8dce2ffda830c1c3f489e7fe10bcec
vabhi <cvabhi94@gmail.com>
2021-06-24 13:04:16 -0700

    switch condition

--- scope.js
+++ scope.js
@@ -1,26 +1,51 @@
-// var nam = "heloo"
+var nam = "heloo"
 
-// function fun() {
-//     var nam = "abhi";
-//     console.log(1, nam);
+function fun() {
+    var nam = "abhi";
+    console.log(1, nam);
+}
+
+function funner() {
+    var nam = "ram";
+    console.log(2, nam);
+}
+
+function funniest() {
+    nam = "hiii";
+    console.log(nam);
+}
+console.log(window, nam);
+fun();
+funner();
+funniest();
+//  example
+
+// var a = 2;
+// if (true) {
+//     var a = 5;
+//     alert(a);
 // }
 
-// function funner() {
-//     var nam = "ram";
-//     console.log(2, nam);
-// }
+//  switch javascript condition 
+//  we can use more and more case 
 
-// function funniest() {
-//     nam = "hiii";
-//     console.log(nam);
-// }
-// console.log(window, nam);
-// fun();
-// funner();
-// funniest();
+function moving(direction) {
+    var whathappen;
+    switch (direction) {
+        case "forward":
+            whathappen: "encounter a monster s";
+            break;
+        case "left":
+            whathappen: "found a river";
+            break;
+        case "back":
+            whathappen: "enter home";
+            break;
+        case "right":
+            whathappen: "enter right";
+            break;
 
-var a = 2;
-if (true) {
-    var a = 5;
-    alert(a);
+
+    }
+    return whathappen;
 }
