f923abf643221ebda259a242fcc05a6ea8525828
vabhi <cvabhi94@gmail.com>
2021-06-24 08:30:34 -0700

    add input.value

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-// var button = document.getElementsByTagName("button")[0];
+var button = document.getElementsByTagName("button")[0];
 
-// button.addEventListener("click", function() {
-//     console.log("click");
-// });
-// button.addEventListener("mouseenter", function() {
-//     console.log("menter");
-// });
+button.addEventListener("click", function() {
+    console.log("click");
+});
+button.addEventListener("mouseenter", function() {
+    console.log("menter");
+});
 
 var button = document.getElementById("enter");
 var value = document.getElementById("input");
 
-// button.addEventListener("click", function() {
-//     console.log("click is working");
-// });
+button.addEventListener("click", function() {
+    console.log("click is working");
+});
 
 var ul = document.querySelector("ul");
 
+// button.addEventListener("click", function() {
+//     var li = document.createElement("li");
+//     li.appendChild(document.createTextNode("testing"));
+//     ul.appendChild(li);
+
+// });
+
 button.addEventListener("click", function() {
+    console.log(input.value);
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"));
+    li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
 
-});
+})
