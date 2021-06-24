3897826af112304ee7a1125b62ad15ea47aff864
vabhi <cvabhi94@gmail.com>
2021-06-24 11:08:41 -0700

    scope ecercise

--- scope.js
+++ scope.js
@@ -1,20 +1,26 @@
-var nam = "heloo"
+// var nam = "heloo"
 
-function fun() {
-    var nam = "abhi";
-    console.log(1, nam);
-}
+// function fun() {
+//     var nam = "abhi";
+//     console.log(1, nam);
+// }
 
-function funner() {
-    var nam = "ram";
-    console.log(2, nam);
-}
+// function funner() {
+//     var nam = "ram";
+//     console.log(2, nam);
+// }
 
-function funniest() {
-    nam = "hiii";
-    console.log(nam);
+// function funniest() {
+//     nam = "hiii";
+//     console.log(nam);
+// }
+// console.log(window, nam);
+// fun();
+// funner();
+// funniest();
+
+var a = 2;
+if (true) {
+    var a = 5;
+    alert(a);
 }
-console.log(window, nam);
-fun();
-funner();
-funniest();
