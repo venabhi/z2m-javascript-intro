693816b90668d93e5b6e34f1caa00f99e63a561b
vabhi <cvabhi94@gmail.com>
2021-06-24 07:56:02 -0700

    dom event listener

---
+++ script.js
@@ -0,0 +1,15 @@
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("click", function() {
+//     console.log("click");
+// });
+// button.addEventListener("mouseenter", function() {
+//     console.log("menter");
+// });
+
+var button = document.getElementById("enter");
+var value = document.getElementById("input");
+
+button.addEventListener("click", function() {
+    console.log("click is working");
+});
