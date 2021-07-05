54c2e7927645ddc3d1dd52fb02b8d2ccffd8c404
vabhi <cvabhi94@gmail.com>
2021-07-05 11:54:21 -0500

    sample react

---
+++ mrr/src/index.js
@@ -0,0 +1,32 @@
+import react from 'react';
+import reactDom from 'react-dom';
+
+//  create a react component a fun / class
+
+const myr = () => {
+  return (
+    <div> welcome to my react </div>
+  );
+}
+
+//  dispaly the component on the screen
+
+// reactDom.render(
+//   <myr / > ,
+//     document.querySelector("#root")
+// );
+
+// const rrr = () => {
+//   return (
+//     <div>
+//       <label className='label'>Firstname</label>
+//       <input id="name" type="text" />
+//       <button  style="background-color: blue; color:white"  />
+//     </div>
+//   );
+// }
+
+// reactDom.render(
+//   <rrr />,
+//   document.querySelector('#root')
+// );
