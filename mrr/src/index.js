b8116b8502b92613211d90b7a5f71d0241dbdde3
vabhi <cvabhi94@gmail.com>
2021-07-05 12:22:38 -0500

    referencing js with jsx

--- mrr/src/index.js
+++ mrr/src/index.js
@@ -9,12 +9,12 @@ const myr = () => {
   );
 }
 
-//  dispaly the component on the screen
+ dispaly the component on the screen
 
-// reactDom.render(
-//   <myr / > ,
-//     document.querySelector("#root")
-// );
+reactDom.render(
+  <myr / > ,
+    document.querySelector("#root")
+);
 
 // const rrr = () => {
 //   return (
@@ -22,11 +22,57 @@ const myr = () => {
 //       <label className='label'>Firstname</label>
 //       <input id="name" type="text" />
 //       <button  style="background-color: blue; color:white"  />
+// style = "background-color: red; color:red;"
+  //  style = {{backgroundColor: "red", color: "blue", font: "10px"}}
+//  style is different in jsx and html 
+//  style = "background-color: blue";
+//  in jsx style = {{backgroundColor : "blue"}}
 //     </div>
-//   );
+//   );S
 // }
 
 // reactDom.render(
 //   <rrr />,
 //   document.querySelector('#root')
 // );
+
+//  referncing  inside js variables to the jsx 
+const app = () => {
+  const vars = "clickme";
+  return (
+    <div>
+       <label className='label'>Firstname</label>
+//       <input id="name" type="text" />
+//       <button  style="background-color: blue; color:white" > {clickme} </button>
+// style = "background-color: red; color:red;"
+  //  style = {{backgroundColor: "red", color: "blue", font: "10px"}}
+//  style is different in jsx and html 
+//  style = "background-color: blue";
+//  in jsx style = {{backgroundColor : "blue"}}
+    </div>
+
+  );
+}
+
+//  referencing outside  js functions in the jsx by calling th function
+
+function abhi() {
+  return "clickme";
+}
+
+const app = () => {
+  const vars = "clickme";
+  return (
+    <div>
+       <label className='label'>Firstname</label>
+//       <input id="name" type="text" />
+//       <button  style="background-color: blue; color:white" > {abhi()} </button>
+// style = "background-color: red; color:red;"
+  //  style = {{backgroundColor: "red", color: "blue", font: "10px"}}
+//  style is different in jsx and html 
+//  style = "background-color: blue";
+//  in jsx style = {{backgroundColor : "blue"}}
+    </div>
+
+  );
+}
