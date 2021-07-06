a791618300ab916bc148c0f6e57678a2ed374e06
vabhi <cvabhi94@gmail.com>
2021-07-06 10:35:17 -0500

    class-component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,14 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //  to get the current location we need to call the api of geolocatio site
+//  we can configure the locationj properties and re-render the app
+//  using the fun we cant re-render the current app the option is class method
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
 
-  return <div>Hi there</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition((position) =>
+      console.log(position)
+    );
+
+    return <div>Latitude:</div>;
+  }
+}
+
+//   return <div>Latitude:</div>;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+// )
+
+// window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(postion),
+//     (err) => console.log(err)
+// );
