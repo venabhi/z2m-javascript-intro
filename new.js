0b1e3c67a1949420ee0a0835504f7d41598506b7
vabhi <cvabhi94@gmail.com>
2021-07-12 17:48:14 -0500

    dot-notation

--- new.js
+++ new.js
@@ -9,9 +9,21 @@ person.lastName;
 
 const news = {
     name:"ram",
+    age=90,
     numbers:[1,2,3,4,'hello'],
     greetings: function () {
-        console.log('hello';)
+        console.log('hello');
     }
 };
 console.log(news.name)
+console.log(news.numbers[1]);
+
+const age = console.log(age);
+console.log(age);
+
+news.greetings ();
+
+//  assign new valus
+news.name= 'venkata';
+
+
